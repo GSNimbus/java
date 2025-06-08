9831f28a3265676a843c9ac95a77e54e4ee29c79
gustavodscruz <gustavodiasdsc@gmail.com>
2025-06-08 16:57:34 -0300

    refactor: update GrupoLocalizacaoRepository and GrupoLocalizacaoService to improve query methods and return types

--- src/main/java/com/gsnimbus/api/repository/GrupoLocalizacaoRepository.java
+++ src/main/java/com/gsnimbus/api/repository/GrupoLocalizacaoRepository.java
@@ -2,18 +2,17 @@ package com.gsnimbus.api.repository;
 
 import java.util.List;
 
-import com.gsnimbus.api.dto.localizacao.grupo.CasaGrupoProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.gsnimbus.api.model.Endereco;
+import com.gsnimbus.api.dto.localizacao.grupo.CasaGrupoProjection;
 import com.gsnimbus.api.model.GrupoLocalizacao;
 
 @Repository
 public interface GrupoLocalizacaoRepository extends JpaRepository<GrupoLocalizacao, Long> {
     List<GrupoLocalizacao> findAllByUsuario_Id(Long usuarioId);
 
-    @Query("select gl.endereco as endereco, gl.id as idGrupoLocalizacao, gl.nome as nome from GrupoLocalizacao gl where gl.usuario.id = :idUsuario ORDER BY gl.usuario.id ASC")
-    CasaGrupoProjection findHomeUsuario(Long idUsuario);
+    @Query("select gl.endereco as endereco, gl.id as idGrupoLocalizacao, gl.nome as nome from GrupoLocalizacao gl where gl.usuario.id = :idUsuario ORDER BY gl.id ASC")
+    List<CasaGrupoProjection> findProjectionsByUsuarioIdOrderedById(Long idUsuario);
 }
--- src/main/java/com/gsnimbus/api/service/GrupoLocalizacaoService.java
+++ src/main/java/com/gsnimbus/api/service/GrupoLocalizacaoService.java
@@ -1,19 +1,20 @@
 package com.gsnimbus.api.service;
 
-import com.gsnimbus.api.dto.localizacao.grupo.CasaGrupoProjection;
-import com.gsnimbus.api.dto.localizacao.grupo.GrupoLocalizacaoDto;
-import com.gsnimbus.api.dto.localizacao.grupo.GrupoLocalizacaoMapper;
-import com.gsnimbus.api.exception.ResourceNotFoundException;
-import com.gsnimbus.api.model.Endereco;
-import com.gsnimbus.api.model.GrupoLocalizacao;
-import com.gsnimbus.api.repository.GrupoLocalizacaoRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.gsnimbus.api.dto.localizacao.grupo.CasaGrupoProjection;
+import com.gsnimbus.api.dto.localizacao.grupo.GrupoLocalizacaoDto;
+import com.gsnimbus.api.dto.localizacao.grupo.GrupoLocalizacaoMapper;
+import com.gsnimbus.api.exception.ResourceNotFoundException;
+import com.gsnimbus.api.model.GrupoLocalizacao;
+import com.gsnimbus.api.repository.GrupoLocalizacaoRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -42,7 +43,11 @@ public class GrupoLocalizacaoService {
     @Cacheable(value = "findHomeUsuario", key = "idUsuario")
     @Transactional(readOnly = true)
     public CasaGrupoProjection findHomeUsuario(Long idUsuario) {
-        return grupoLocalizacaoRepository.findHomeUsuario(idUsuario);
+        List<CasaGrupoProjection> projections = grupoLocalizacaoRepository.findProjectionsByUsuarioIdOrderedById(idUsuario);
+        if (projections.isEmpty()){
+            return null; // Ou lançar uma exceção se um resultado for sempre esperado
+        }
+        return projections.get(0); // Retorna o primeiro elemento da lista
     }
 
     @Transactional
